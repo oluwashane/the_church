18adf94148e57f22b245c606d9c73c5948a0f386
oluwashane <ogbulaforudochukwu@gmail.com>
2020-11-19 22:53:16 +0100

    update101

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown';
 import '../assets/styles/navbar.css'
-import churchLogo from '../assets/images/jdaLogo.png'
 
 
 const Navbar = () => {
